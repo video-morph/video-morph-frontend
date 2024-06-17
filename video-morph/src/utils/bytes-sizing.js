180ef0f17e10696d2f27016e3c4c977cde61303f
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-06-16 17:42:02 -0700

    started working on new implementation for transcoding ffeature

---
+++ video-morph/src/utils/bytes-sizing.js
@@ -0,0 +1,11 @@
+export default function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  
+    if (bytes === 0) return '0 Byte';
+  
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    const size = (bytes / Math.pow(1024, i)).toFixed(2);
+  
+    return `${size} ${sizes[i]}`;
+  }
+  
---
+++ video-morph/src/utils/conversion.js
@@ -0,0 +1,66 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
+
+function getFileExtension(file_name) {
+  const regex = /(?:\.([^.]+))?$/; // Matches the last dot and everything after it
+  const match = regex.exec(file_name);
+  if (match && match[1]) {
+    return match[1];
+  }
+  return ""; // No file extension found
+}
+
+function removeFileExtension(file_name) {
+  const lastDotIndex = file_name.lastIndexOf(".");
+  if (lastDotIndex !== -1) {
+    return file_name.slice(0, lastDotIndex);
+  }
+  return file_name; // No file extension found
+}
+
+export default async function convert(ffmpeg, action) {
+  const ffmpeg = new FFmpeg();
+  await ffmpeg.load();
+
+  const { file, to, file_name, file_type } = action;
+  const input = getFileExtension(file_name);
+  const output = removeFileExtension(file_name) + "." + to;
+  ffmpeg.writeFile(input, await fetchFile(file));
+
+  // FFmpeg commands
+  let ffmpeg_cmd = [];
+  // 3gp video
+  if (to === "3gp") {
+    ffmpeg_cmd = [
+      "-i",
+      input,
+      "-r",
+      "20",
+      "-s",
+      "352x288",
+      "-vb",
+      "400k",
+      "-acodec",
+      "aac",
+      "-strict",
+      "experimental",
+      "-ac",
+      "1",
+      "-ar",
+      "8000",
+      "-ab",
+      "24k",
+      output,
+    ];
+  } else {
+    ffmpeg_cmd = ["-i", input, output];
+  }
+
+  // Execute command
+  await ffmpeg.exec(ffmpeg_cmd);
+
+  const data = await ffmpeg.readFile(output);
+  const blob = new Blob([data], { type: file_type.split("/")[0] });
+  const url = URL.createObjectURL(blob);
+  return { url, output };
+}
