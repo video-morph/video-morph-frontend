c233c952345bfa1c129c5e602902e9c7a213a606
Abdussalam Mustapha <103737038+abdussalam-mustapha@users.noreply.github.com>
2024-06-18 04:34:12 -0700

    feat: added different UI components

---
+++ video-morph/src/components/dropzone/dropzone.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+
+const Dropzone = () => {
+ 
+
+  return (
+    <div>
+   
+    </div>
+  );
+};
+
+export default Dropzone;
