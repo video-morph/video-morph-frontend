3a7f00a7e99d44a5232595e5a793ef7e856d816b
Onasanya Daniel <91058251+Onas-2000@users.noreply.github.com>
2024-06-21 15:01:57 +0200

    Remove quotation causing bug during deployment

--- video-morph/src/app/page.js
+++ video-morph/src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="w-[80%] flex flex-col justify-center items-center">
         <p className="text-4xl">Convert your Video Format Online Easily</p>
         <p className="text-center">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         </p>
       </div>
